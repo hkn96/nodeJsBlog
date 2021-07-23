cad2738f6a444960eb0d385134c7048aaa728c3d
Hakan Ünlü <unluhak@outlook.com>
2021-07-24 00:57:39 +0200

    template engine 'ejs' added

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
 
+// template engine
+app.set('view engine', 'ejs');
+
 app.listen(3000);
 
 app.get('/', (req, res) => {
-  res.sendFile('./views/index.html', { root: __dirname });
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
-  res.sendFile('./views/about.html', { root: __dirname });
+  res.render('about');
 });
 
 app.use((req, res) => {
